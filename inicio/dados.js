c1a96da7b0d071ad43639374fe0f30b5a204181a
ecristinamendessoares <ecristinamendessares@gmail.com>
2026-02-27 11:53:42 -0300

    tipos de dados em js

---
+++ inicio/dados.js
@@ -0,0 +1,13 @@
+console.log(19.9 + 0,6);
+
+var preco = 19.9;
+var desconto = 0.6;
+var precoComDesconto = preco * (1 - desconto);
+console.log(precoComdesconto);
+
+let nome = "Caderno";
+let categoria = "Papelaria"
+console.log("Produtos" + nome +
+", Categoria: " + nome +
+
+
