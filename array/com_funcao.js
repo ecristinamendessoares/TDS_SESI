9c41004155fe41e5f6cca307790d1898a1d67067
ecristinamendessoares <ecristinamendessoares@gmail.com>
2026-03-19 12:22:13 -0300

    Functions em JS

---
+++ array/com_funcao.js
@@ -0,0 +1,14 @@
+
+
+const produto = "Notebook";
+const preco1 = 3500;
+
+function exibirProduto(){
+    console.log(`${produto1} -> ${preco1}`);
+}
+const produto2 = "pelucia";
+const preco2 = 150;
+
+function exibirProduto2(){
+    console.log(`${produto2} -> ${preco2}`);
+}
---
+++ array/sem_funcao.js
@@ -0,0 +1,12 @@
+
+const produto = "Notebook";
+const preco1 = 3500;
+
+console.log(`Produto: $(produto2)`);
+console.log(`Preco: ${preco1}`);
+
+const produto2 = "pelucia";
+const preco2 = 150;
+
+console.log(`Produto: ${produto2}`);
+console.log(`preco: ${preco}`);
